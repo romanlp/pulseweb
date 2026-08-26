8a478eb88581cb11a4ac116eed8b2630ba6c7400
romanlp <roman.lapacherie@gmail.com>
2026-08-26 11:28:37 +0100

    chore(pulseweb): update page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <h1 className="text-4xl font-bold">Hello</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
     </div>
-  )
+  );
 }
